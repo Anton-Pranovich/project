edef9f8488de02535cdc1dfe496f85feedcac31a
Anton <Anton@LAPTOP-RTSEHHDE>
2021-06-03 16:43:35 +0300

    Add new tariffs

--- src/GUI/ModelTariff.java
+++ src/GUI/ModelTariff.java
@@ -7,97 +7,104 @@ import model.Tariff;
 import javax.swing.table.AbstractTableModel;
 
 public class ModelTariff extends AbstractTableModel {
-	
-	    /**
-	 * 
-	 */
+
+	/**
+	* 
+	*/
 	private static final long serialVersionUID = -8855614213849594120L;
-		List<Tariff> tariff = new ArrayList<>();
-	    String colNames[] = { "Name", "operator name", "payroll","callPricesWithinTheNetwork","callPricesOutsideTheNetwork", "callPricesеToHomeNetwork","smsPrice","parameters" };
-	    Class<?> colClasses[] = { String.class, String.class, Double.class, Double.class,Double.class,Double.class,Double.class,String.class };
-	    
-	    ModelTariff() {
-	    	tariff.add(new Tariff("name", "operator name", 10, 0.1,0.8,0.8,0.2,"parameters"));
-	    	tariff.add(new Tariff("name1", "operator name", 10, 0.1,0.8,0.8,0.2,"parameters"));
-	    	tariff.add(new Tariff("name2", "operator name", 10, 0.1,0.8,0.8,0.2,"parameters"));
-	    	tariff.add(new Tariff("name3", "operator name", 10, 0.1,0.8,0.8,0.2,"parameters"));
-	    }
-	    
-	    public int getRowCount() {
-	        return tariff.size();
-	    }
+	List<Tariff> tariff = new ArrayList<>();
+	String colNames[] = { "Name", "operator name", "payroll", "callPricesWithinTheNetwork",
+			"callPricesOutsideTheNetwork", "callPricesеToHomeNetwork", "smsPrice", "parameters" };
+	Class<?> colClasses[] = { String.class, String.class, Double.class, Double.class, Double.class, Double.class,
+			Double.class, String.class };
 
-	    public int getColumnCount() {
-	        return colNames.length;
-	    }
-
-	    public Object getValueAt(int rowIndex, int columnIndex) {
-	        if (columnIndex == 0) {
-	            return tariff.get(rowIndex).getName();
-	        }
-	        if (columnIndex == 1) {
-	            return tariff.get(rowIndex).getOperatorName();
-	        }
-	        if (columnIndex == 2) {
-	            return tariff.get(rowIndex).getPayroll();
-	        }
-	        if (columnIndex == 3) {
-	            return tariff.get(rowIndex).getCallPricesWithinTheNetwork();
-	        }
-	        if (columnIndex == 4) {
-	            return tariff.get(rowIndex).getCallPricesOutsideTheNetwork();
-	        }
-	        if (columnIndex == 5) {
-	            return tariff.get(rowIndex).getCallPricesеToHomeNetwork();
-	        }
-	        if (columnIndex == 6) {
-	            return tariff.get(rowIndex).getSmsPrice();
-	        }
-	        if (columnIndex == 7) {
-	            return tariff.get(rowIndex).getParameters();
-	        }
-	        return null;
-	    }
-
-	    public String getColumnName(int columnIndex) {
-	        return colNames[columnIndex];
-	    }
-
-	    public Class<?> getColumnClass(int columnIndex) {
-	        return colClasses[columnIndex];
-	    }
-
-	    public boolean isCellEditable(int rowIndex, int columnIndex) {
-	        return true;
-	    }
-
-	    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-	        if (columnIndex == 0) {
-	        	tariff.get(rowIndex).setName((String) aValue);
-	        }
-	        if (columnIndex == 1) {
-	        	tariff.get(rowIndex).setOperatorName((String) aValue);
-	        }
-	        if (columnIndex == 2) {
-	        	tariff.get(rowIndex).setPayroll((double) aValue);
-	        }
-	        if (columnIndex == 3) {
-	        	tariff.get(rowIndex).setCallPricesWithinTheNetwork((double) aValue);
-	        }
-	        if (columnIndex == 4) {
-	        	tariff.get(rowIndex).setCallPricesOutsideTheNetwork((double) aValue);
-	        }
-	        if (columnIndex == 5) {
-	        	tariff.get(rowIndex).setCallPricesеToHomeNetwork((double) aValue);
-	        }
-	        if (columnIndex == 6) {
-	        	tariff.get(rowIndex).setSmsPrice((double) aValue);
-	        }
-	        if (columnIndex == 7) {
-	        	tariff.get(rowIndex).setParameters((String) aValue);
-	        }
-	        fireTableCellUpdated(rowIndex, columnIndex);
-	        
-	    }
+	ModelTariff() {
+		tariff.add(new Tariff("КомфортМ", "A1", 26.9, 0.0, 0.1, 0.1, 0.078,
+				"Абонентская плата для новых абонентов с учетом скидки:\r\n" + "Первые 3 месяца - 13,90 руб./мес."));
+		tariff.add(new Tariff("Привет", "A1", 0, 0.104, 0.104, 0.104, 0.104, "0,00 руб/мес абонентская плата"));
+		tariff.add(new Tariff("БизнесКласс", "A1", 125.17, 0.0, 0.0, 0.0, 0.0, "Почувствуйте уверенность в завтрашнем дне"));
+		tariff.add(new Tariff("Безлимитище", "МТС", 25.48, 0.0468, 0.0988, 0.0988, 0.0780, "Безлимит интернета!, Бесплатная подписка на кино и ТВ "));
+		tariff.add(new Tariff("СуперГолос", "МТС", 24.98, 0.0, 0.0, 0.0, 0.0780, "Безлимит звонков во все сети Республики Беларусь "));
+		tariff.add(new Tariff("Абсолют", "МТС", 0.0, 0.0, 0.0, 0.0, 0.0, "200 минут на международные звонки, Безлимитные звонки во все сети, интернет, SMS и MMS"));
+		tariff.add(new Tariff("Шейк", "Life", 14.90, 0.0, 0.15, 0.15, 0.007, "Бесплатная раздача трафика, Накопление ГБ и минут, Безлимит на мессенджеры"));
+		tariff.add(new Tariff("ВсеВключено", "Life", 21.90, 0.0, 0.0, 0.0, 0.0, "Безлимит во все сети, Безлимит ГБ,Бесплатная раздача трафика"));
+		tariff.add(new Tariff("Геймер", "Life", 22.90, 0.0, 0.15, 0.15, 0.07, "Безлимит интернета!, 300 коинов на покупку игр в Steam "));
 	}
 
+	public int getRowCount() {
+		return tariff.size();
+	}
+
+	public int getColumnCount() {
+		return colNames.length;
+	}
+
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		if (columnIndex == 0) {
+			return tariff.get(rowIndex).getName();
+		}
+		if (columnIndex == 1) {
+			return tariff.get(rowIndex).getOperatorName();
+		}
+		if (columnIndex == 2) {
+			return tariff.get(rowIndex).getPayroll();
+		}
+		if (columnIndex == 3) {
+			return tariff.get(rowIndex).getCallPricesWithinTheNetwork();
+		}
+		if (columnIndex == 4) {
+			return tariff.get(rowIndex).getCallPricesOutsideTheNetwork();
+		}
+		if (columnIndex == 5) {
+			return tariff.get(rowIndex).getCallPricesеToHomeNetwork();
+		}
+		if (columnIndex == 6) {
+			return tariff.get(rowIndex).getSmsPrice();
+		}
+		if (columnIndex == 7) {
+			return tariff.get(rowIndex).getParameters();
+		}
+		return null;
+	}
+
+	public String getColumnName(int columnIndex) {
+		return colNames[columnIndex];
+	}
+
+	public Class<?> getColumnClass(int columnIndex) {
+		return colClasses[columnIndex];
+	}
+
+	public boolean isCellEditable(int rowIndex, int columnIndex) {
+		return true;
+	}
+
+	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+		if (columnIndex == 0) {
+			tariff.get(rowIndex).setName((String) aValue);
+		}
+		if (columnIndex == 1) {
+			tariff.get(rowIndex).setOperatorName((String) aValue);
+		}
+		if (columnIndex == 2) {
+			tariff.get(rowIndex).setPayroll((double) aValue);
+		}
+		if (columnIndex == 3) {
+			tariff.get(rowIndex).setCallPricesWithinTheNetwork((double) aValue);
+		}
+		if (columnIndex == 4) {
+			tariff.get(rowIndex).setCallPricesOutsideTheNetwork((double) aValue);
+		}
+		if (columnIndex == 5) {
+			tariff.get(rowIndex).setCallPricesеToHomeNetwork((double) aValue);
+		}
+		if (columnIndex == 6) {
+			tariff.get(rowIndex).setSmsPrice((double) aValue);
+		}
+		if (columnIndex == 7) {
+			tariff.get(rowIndex).setParameters((String) aValue);
+		}
+		fireTableCellUpdated(rowIndex, columnIndex);
+
+	}
+}
